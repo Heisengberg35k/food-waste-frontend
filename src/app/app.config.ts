acc7ab0e1b0d0fb406824fc8c08b2224f2c7d110
Khaled Mahmud <mahmud.khaled365@gmail.com>
2026-03-28 01:35:21 +0000

    Implemented login API integration successfully

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
-import { provideHttpClient } from '@angular/common/http';
-
-export const appConfig: ApplicationConfig = {
+export const appConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()
+    provideRouter(routes),            
+    provideHttpClient(withFetch())
   ]
 };
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,19 +15,36 @@ export class LoginComponent {
   password = '';
   message = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   onLogin() {
     const data = {
-      email: this.email,
-      password: this.password
+      email: this.email.trim(),
+      password: this.password.trim()
     };
 
     this.authService.login(data).subscribe({
       next: (res: any) => {
-        this.message = 'Login successful!';
+        console.log("RESPONSE:", res);
+
+        if (res.access_token) {
+          // Store JWT token
+          localStorage.setItem('token', res.access_token);
+
+          // Show success message
+          this.message = 'Login successful!';
+
+          // Redirect to home page
+          this.router.navigate(['/']);
+        } else {
+          this.message = 'Login failed!';
+        }
       },
-      error: () => {
+      error: (err) => {
+        console.error("ERROR:", err);
         this.message = 'Login failed!';
       }
     });
